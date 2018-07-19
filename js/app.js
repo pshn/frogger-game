abc39f8c31b38bce65b5d9ad96417e69ebe142e9
sylveelee <sylvee.lee@eikospartners.com>
2018-07-19 16:06:48 -0400

    Created prototypes for enemy and player

--- js/app.js
+++ js/app.js
@@ -24,7 +24,29 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+  this.sprite = 'images/char-boy.png';
+};
 
+Player.prototype.update = function(dt) {
+
+};
+
+Player.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function() {
+  console.log('hi')
+};
+
+let enemy = new Enemy;
+let player = new Player;
+
+console.log('what is this', enemy)
+
+let allEnemies = [];
+allEnemies.push(enemy);
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
